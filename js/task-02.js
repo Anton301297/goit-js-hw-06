76e571c66e01862cb3d8a1801fb76168c5adaeec
Anton <polusmak9999@gmal.com>
2022-11-05 10:34:39 +0300

    task 1

---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const itemEl = document.createElement("li");
+console.log(itemEl);
+itemEl.classList.add("item");
